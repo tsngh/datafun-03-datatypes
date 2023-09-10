504308e6efc805199cced23889c7f5b910f5e70a
Tesheena <singh.tesheena27@gmail.com>
2023-09-09 19:39:50 -0500

    initial data

--- user_numeric_lists.py
+++ user_numeric_lists.py
@@ -5,13 +5,22 @@ Modify this docstring.
 
 # import some standard modules first - how many can you make use of?
 import math
-
+import statistics
 
 # TODO: import from local util_datafun_logger.py 
 
+from util_datafun_logger import setup_logger
+
 # TODO: Call the setup_logger function to create a logger and get the log file name
 
+logger, logname = setup_logger(__file__)
+
 # TODO: Create some shared data lists if you like - or just create them in your functions
+
+list1 = [23, 11, 14, 76, 61, 10, 32, 56, 77, 77, 87, 150, 167, 201, 221, 245, 307, 11, 61, 90 ]
+
+listx = list(range(10))
+listy = [1, 3, 5, 7, 8, 9, 11, 11, 15, 16]
 
 # TODO: define some custom functions
 
